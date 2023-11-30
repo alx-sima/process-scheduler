457cb95717bfd4ec7ff9fd7a7b43944dfe41476b
Alexandru Sima <alexsima203@gmail.com>
2023-11-30 17:22:48 +0200

    Refactor code

--- scheduler/src/schedulers/round_robin.rs
+++ scheduler/src/schedulers/round_robin.rs
@@ -11,6 +11,7 @@ struct ProcessMeta {
     pid: Pid,
     state: ProcessState,
     last_update: usize,
+    priority: i8,
     timings: (
         // Total
         usize,
@@ -22,9 +23,10 @@ struct ProcessMeta {
 }
 
 impl ProcessMeta {
-    fn new(pid: Pid, creation_time: usize) -> Self {
+    fn new(pid: Pid, priority: i8, creation_time: usize) -> Self {
         ProcessMeta {
             pid,
+            priority,
             last_update: creation_time,
             state: ProcessState::Ready,
             timings: (0, 0, 0),
@@ -46,7 +48,7 @@ impl Process for ProcessMeta {
     }
 
     fn priority(&self) -> i8 {
-        0
+        self.priority
     }
 
     fn extra(&self) -> String {
@@ -60,7 +62,6 @@ struct CurrentProcessMeta {
     execution_cycles: usize,
     remaining_timeslice: usize,
     syscall_cycles: usize,
-    schedule_time: usize,
 }
 
 pub struct RoundRobin {
@@ -89,26 +90,18 @@ impl RoundRobin {
             will_panic: false,
         }
     }
-}
 
-impl Scheduler for RoundRobin {
-    fn next(&mut self) -> SchedulingDecision {
-        update(&mut self.processes, &mut self.sleep_timer, self.clock);
-        if self.will_panic {
-            return SchedulingDecision::Panic;
-        }
-        if let Some(current) = &self.current_process {
-            return SchedulingDecision::Run {
-                pid: current.process.pid(),
-                timeslice: NonZeroUsize::new(current.remaining_timeslice).unwrap(),
-            };
-        }
+    fn get_processes(&self) -> Vec<&ProcessMeta> {
+        self.processes
+            .iter()
+            .chain(self.sleep_timer.iter().map(|(x, _)| x))
+            .collect()
+    }
 
-        // loop {
-        let (awaken, sleeping): (VecDeque<_>, VecDeque<_>) = self
-            .sleep_timer
-            .drain(..)
-            .partition(|(_, wake_time)| *wake_time <= self.clock);
+    /// Wake up all processes that finished waiting.
+    fn wake_processes(&mut self) {
+        let sleeping = self.sleep_timer.drain(..);
+        let (awaken, sleeping) = sleeping.partition(|(_, wake_time)| *wake_time <= self.clock);
         self.sleep_timer = sleeping;
 
         let awaken = awaken.into_iter().map(|(process, _)| ProcessMeta {
@@ -116,51 +109,90 @@ impl Scheduler for RoundRobin {
             ..process
         });
         self.processes.extend(awaken);
+    }
 
-        if let Some(index) = self.processes.iter().enumerate().find_map(|(index, x)| {
-            if x.state == ProcessState::Ready {
-                Some(index)
-            } else {
-                None
-            }
-        }) {
-            if let Some(mut process) = self.processes.remove(index) {
+    fn get_next_process(&mut self) -> Option<CurrentProcessMeta> {
+        let mut waiting_processes = VecDeque::new();
+
+        while let Some(mut process) = self.processes.pop_front() {
+            if process.state == ProcessState::Ready {
                 process.state = ProcessState::Running;
-                let pid = process.pid();
-
-                self.current_process = Some(CurrentProcessMeta {
+                self.processes.extend(waiting_processes);
+                return Some(CurrentProcessMeta {
                     process,
                     remaining_timeslice: self.timeslice.get(),
                     execution_cycles: 0,
                     syscall_cycles: 0,
-                    schedule_time: self.clock,
                 });
-
-                return SchedulingDecision::Run {
-                    pid,
-                    timeslice: self.timeslice,
-                };
             }
-        } else {
-            // There aren't any ready processes, wait for the first to wake up.
-            if let Some(first_wake) = self
-                .sleep_timer
-                .iter()
-                .map(|(_, wake_time)| wake_time)
-                .min()
-            {
-                let wait_interval = first_wake - self.clock;
-                println!("wait for {}", wait_interval);
-                self.clock = *first_wake;
+            waiting_processes.push_back(process);
+        }
+        self.processes = waiting_processes;
+        None
+    }
 
-                return SchedulingDecision::Sleep(NonZeroUsize::new(wait_interval).unwrap());
+    fn update_timings(&mut self) {
+        let p = &mut self.processes;
+        let s = &mut self.sleep_timer;
+        let time = self.clock;
+        let s = s.into_iter().map(|(process, _)| process);
+        for i in p.iter_mut().chain(s) {
+            let elapsed_time = time - i.last_update;
+            i.last_update = time;
+
+            i.timings.0 += elapsed_time;
+            if i.state == ProcessState::Running {
+                i.timings.2 += elapsed_time;
+            }
+        }
+    }
+}
+
+impl Scheduler for RoundRobin {
+    fn next(&mut self) -> SchedulingDecision {
+        self.update_timings();
+
+        if self.will_panic {
+            return SchedulingDecision::Panic;
+        }
+
+        if let Some(current) = &self.current_process {
+            return SchedulingDecision::Run {
+                pid: current.process.pid(),
+                timeslice: NonZeroUsize::new(current.remaining_timeslice).unwrap(),
+            };
+        }
+
+        self.wake_processes();
+        self.current_process = self.get_next_process();
+
+        if let Some(current) = &self.current_process {
+            return SchedulingDecision::Run {
+                pid: current.process.pid(),
+                timeslice: NonZeroUsize::new(current.remaining_timeslice).unwrap(),
+            };
+        }
+
+        // There aren't any ready processes, wait for the first to wake up.
+        if let Some(first_wake) = self
+            .sleep_timer
+            .iter()
+            .map(|(_, wake_time)| wake_time)
+            .min()
+        {
+            let wait_interval = first_wake - self.clock;
+            self.clock = *first_wake;
+
+            return SchedulingDecision::Sleep(NonZeroUsize::new(wait_interval).unwrap());
+        } else {
+            // There are no processes to be awaken. If there still
+            // are processes waiting, signal a deadlock.
+            if self.processes.len() != 0 {
+                return SchedulingDecision::Deadlock;
             } else {
-                // No process is ready to wake up.
                 return SchedulingDecision::Done;
             }
         }
-        // }
-        SchedulingDecision::Done
     }
 
     fn stop(&mut self, reason: StopReason) -> SyscallResult {
@@ -173,12 +205,11 @@ impl Scheduler for RoundRobin {
                     current.process.timings.1 += 1;
                     current.syscall_cycles += 1;
                 }
-                println!("clock {:?}", self.clock);
 
                 let syscall_result = match syscall {
-                    Fork(_) => {
+                    Fork(priority) => {
                         self.max_pid += 1;
-                        let new_process = fork(self.max_pid, self.clock);
+                        let new_process = fork(self.max_pid, priority, self.clock);
                         let new_pid = new_process.pid();
                         self.processes.push_back(new_process);
 
@@ -188,7 +219,9 @@ impl Scheduler for RoundRobin {
                         if let Some(current) = self.current_process.take() {
                             // Killing `init` while other processes are
                             // running will result in a panic.
-                            if current.process.pid() == 1 && self.processes.len() != 0 {
+                            if current.process.pid() == 1
+                                && (self.processes.len() != 0 || self.sleep_timer.len() != 0)
+                            {
                                 self.will_panic = true;
                             }
 
@@ -214,8 +247,10 @@ impl Scheduler for RoundRobin {
                     Wait(event) => {
                         if let Some(mut current) = self.current_process.take() {
                             current.process.state = ProcessState::Waiting { event: Some(event) };
-                            current.execution_cycles +=
-                                self.clock - current.process.last_update - current.syscall_cycles;
+                            let execution_time = self.clock - current.process.last_update;
+                            if execution_time > current.syscall_cycles {
+                                current.execution_cycles += execution_time - current.syscall_cycles;
+                            }
                             current.process.timings.0 += current.execution_cycles;
                             current.process.timings.2 += current.execution_cycles;
                             current.process.last_update = self.clock;
@@ -242,7 +277,6 @@ impl Scheduler for RoundRobin {
                             process.timings.2 += execution_cycles;
                             process.last_update = self.clock;
 
-                            print!("sleeping until{}", self.clock + time);
                             self.sleep_timer.push_back((process, self.clock + time));
                             SyscallResult::Success
                         } else {
@@ -253,7 +287,6 @@ impl Scheduler for RoundRobin {
 
                 // Update the timer.
                 if let Some(CurrentProcessMeta {
-                    schedule_time: _,
                     execution_cycles,
                     syscall_cycles,
                     remaining_timeslice,
@@ -261,23 +294,21 @@ impl Scheduler for RoundRobin {
                     ..
                 }) = self.current_process.as_mut()
                 {
-                    println!("{} {} {}", *remaining_timeslice, *syscall_cycles, remaining);
-                    *execution_cycles += *remaining_timeslice - (*syscall_cycles + remaining);
+                    let execution_time = *remaining_timeslice - remaining;
+                    if execution_time > *syscall_cycles {
+                        *execution_cycles += execution_time - *syscall_cycles;
+                    }
                     *remaining_timeslice = remaining;
                     process.last_update = self.clock;
                 }
-                update(&mut self.processes, &mut self.sleep_timer, self.clock);
+                self.update_timings();
 
                 if let Some(mut current) = self.current_process.take() {
                     if current.remaining_timeslice < self.minimum_remaining_timeslice {
                         current.process.state = ProcessState::Ready;
                         current.process.timings.0 += current.execution_cycles;
                         current.process.timings.2 += current.execution_cycles;
-                        println!(
-                            "Debug: Execution cycles for PID {}: {}",
-                            current.process.pid(),
-                            current.execution_cycles
-                        );
+
                         self.processes.push_back(current.process);
                     } else {
                         self.current_process = Some(current);
@@ -293,7 +324,7 @@ impl Scheduler for RoundRobin {
                     current.process.timings.2 += current.remaining_timeslice;
                     current.process.last_update = self.clock;
 
-                    update(&mut self.processes, &mut self.sleep_timer, self.clock);
+                    self.update_timings();
                     self.processes.push_back(current.process);
 
                     SyscallResult::Success
@@ -311,30 +342,12 @@ impl Scheduler for RoundRobin {
             processes.push(&current.process as &dyn Process);
         }
 
-        [
-            processes,
-            self.processes.iter().map(|x| x as &dyn Process).collect(),
-            self.sleep_timer
-                .iter()
-                .map(|(x, _)| x as &dyn Process)
-                .collect(),
-        ]
-        .concat()
+        processes.extend(self.get_processes().into_iter().map(|x| x as &dyn Process));
+        processes
     }
 }
 
-fn fork(pid: usize, creation_time: usize) -> ProcessMeta {
-    let new_process = ProcessMeta::new(Pid::new(pid), creation_time);
+fn fork(pid: usize, priority: i8, creation_time: usize) -> ProcessMeta {
+    let new_process = ProcessMeta::new(Pid::new(pid), priority, creation_time);
     new_process
-}
-
-fn update(p: &mut VecDeque<ProcessMeta>, s: &mut VecDeque<(ProcessMeta, usize)>, time: usize) {
-    let s = s.into_iter().map(|(process, _)| process);
-    for i in p.iter_mut().chain(s) {
-        i.timings.0 += time - i.last_update;
-        println!("{}: {}", i.pid(), time - i.last_update);
-        println!("updated {}", i.last_update);
-        i.last_update = time;
-    }
-    println!("clock {} update {:?}", time, p);
 }
